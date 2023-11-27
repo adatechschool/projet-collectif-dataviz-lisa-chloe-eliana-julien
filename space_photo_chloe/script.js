d49faede0d1c59944e620a43147f647fb14e0666
QuangHung0 <nguyen.julien2003@gmail.com>
2023-11-27 12:31:10 +0100

    Popup

--- space_photo_chloe/script.js
+++ space_photo_chloe/script.js
@@ -30,3 +30,17 @@ fetch(`${url}?api_key=${api_key}`)
   console.error('Error:', error);
  });
 
+ function popupImage(imageUrl) {
+   // Créer une fenêtre pop-up
+   var popup = window.open('', 'Image Pop-up', 'width=600,height=400');
+
+   // Insérer le code HTML dans la fenêtre pop-up pour afficher l'image
+   popup.document.write('<html><head><title>Image Pop-up</title></head><body style="margin:0;text-align:center;background: #111">');
+   popup.document.write('<img src="' + imageUrl + '" alt="Image" style="width:100%; height:auto; display:block;">');
+   popup.document.write('</body></html>');
+
+   // Fermer le document de la fenêtre pop-up pour éviter tout problème potentiel
+   popup.document.close();
+ }
+
+popupImage(image-space);
