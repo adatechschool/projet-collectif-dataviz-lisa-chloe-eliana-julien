898797e1fb15884ced83be5d42607632ab5e59ec
pchloe02 <146726419+pchloe02@users.noreply.github.com>
2023-11-29 14:47:03 +0100

    galerie 4 images

--- space_photo_chloe/script.js
+++ space_photo_chloe/script.js
@@ -1,27 +1,21 @@
-const url = "https://api.nasa.gov/planetary/apod";
 const api_key = "nyAfB7JXz5pufTPcJBHue2c8DBILeTaHZviBTzEU";
-let nextDay = dateFormat(getNextDay())
-
-function getNextDay() {
-
-   let today = new Date()
-   let nextDay = new Date()
-   nextDay.setDate(today.getDate() - 2)
-   return nextDay
-}
-
-function dateFormat(date) {
-   let year = date.getFullYear()
-   let month = ("0" + (date.getMonth() + 1)).slice(-2)
-   let day = ("0" + date.getDate()).slice(-2)
-   return `${year}-${month}-${day}`
-}
+let images = []
+let date = new Date()
 
 
-fetch(`${url}?api_key=${api_key}`)
+
+//recup de la data
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}`)
  .then(response => {
-  console.log('Response:', response);
-  return response.json();
+   if(response.ok) {
+      console.log("Clé Valide")
+   } else {
+      console.log("Clé non valide")
+   }
+
+
+    console.log('Response:', response);
+    return response.json();
  })
  .then(data => {
     console.log('Data:', data);
@@ -42,26 +36,66 @@ fetch(`${url}?api_key=${api_key}`)
     const publication = data.date
     const addDate = document.getElementById('date')
     addDate.innerHTML= publication
- })
- .catch(error => {
-  console.error('Error:', error);
- });
-
- fetch(`https://api.nasa.gov/planetary/apod?date=${nextDay}&api_key=${api_key}`)
- .then(response => {
-  console.log('Response:', response);
-  return response.json();
- })
- .then(data => {
-    console.log('Data:', data);
-    spaceURL2 = data.url
-
-    document.getElementById('image-space2').src = spaceURL2
-
 
  })
- .catch(error => {
-  console.error('Error:', error);
- });
+   .catch(error => {
+      console.error('Error:', error);
+   });
 
+
+for(let i = 1 ; i < 5 ; i++) {
+      
+   date.setDate(date.getDate() - i) 
+
+   let year = date.getFullYear()
+   let month = ("0" + (date.getMonth() + 1)).slice(-2)
+   let day = ("0" + date.getDate()).slice(-2)
+
+   fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${year}-${month}-${day}`)
+   .then(response => {
+      console.log('Response:', response);
+      return response.json();
+   })
+   .then(data => {
+      console.log('Data:', data);
+      
+      images.push(data.url)
+      let spaceURL2 = images[0]
+      document.getElementById('image-space2').src = spaceURL2
+      
+      let spaceURL3 = images[1]
+      document.getElementById('image-space3').src = spaceURL3
+
+      let spaceURL4 = images[2]
+      document.getElementById('image-space4').src = spaceURL4
+
+      let spaceURL5 = images[3]
+      document.getElementById('image-space5').src = spaceURL5
+   })
+   
+   .catch(error => {
+   });
+}
+      
+
+
+
+
+
+
+
+ 
+ //Pop-upde l'image 1
+ function popupImage() {
+ 
+    var popup = window.open('', 'Image Pop-up', 'width=600,height=400');
+ 
+    popup.document.write('<html><head><title>Image Pop-up</title></head><body style="margin:0;text-align:center;background: #111">');
+    popup.document.write('<img src="' + spaceURL + '" alt="Image" style="width:100%; height:auto; display:block;">');
+    popup.document.write('</body></html>');
+    popup.document.close();
+ }
+    
+    popupImage();
+ 
 
